cfdfeb57e5a0d4c62c068a7ba14643fdc8060720
Uadson Feitosa <uadsonpy@gmail.com>
2022-11-08 17:31:16 -0300

    adds fixtures in conn and session

--- tests/tests_app/test_users.py
+++ tests/tests_app/test_users.py
@@ -1,26 +1,36 @@
+import pytest
 from app.user import User
 from app.database import Connection
     
+
+@pytest.fixture
+def conn():
+    # setup
+    conn_obj = Connection()
+    yield conn_obj
+    # tear down
+    conn_obj.close()
+
+
+@pytest.fixture
+def session(conn):
+    # setup
+    session_obj = conn.get_engine()
+    yield session_obj
+    # tear down
+    session_obj.roll_back()
+    session_obj.close()
     
-def test_user_save():
-    conn = Connection()
-    session = conn.get_engine()
+
+def test_user_save(session):
     user = User(name='Foo')
     session.save(user)
     assert isinstance(user.id, int)
-    session.roll_back()
-    session.close()
-    conn.close()
     
     
-def test_list_users():
-    conn = Connection()
-    session = conn.get_engine()
+def test_list_users(session):
     users = [User(name='Foo'), User(name='Dot')]
     for user in users:
         session.save(user)
     assert users == session.get_list_objects()
-    session.roll_back()
-    session.close()
-    conn.close()
     
