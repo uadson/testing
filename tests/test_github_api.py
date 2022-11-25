d100ca1dce0543d23cd96549ac949347d08bc1e4
Uadson Feitosa <uadsonpy@gmail.com>
2022-11-25 18:38:52 -0300

    refactor test in github_api

--- tests/test_github_api.py
+++ tests/test_github_api.py
@@ -4,18 +4,17 @@ from ..app import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
-    url = "https://avatars.githubusercontent.com/u/62815552?v=4"
+    url = "https://avatars.githubusercontent.com/u/62815553?v=4"
     resp_mock.json.return_value = {
         "login": "uadson",
-        "id": 62815552,
+        "id": 62815553,
         "avatar_url": url,
     }
-    get_main = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_main
+    get_mock=mocker.patch('testing.app.github_api.requests.get')
+    get_mock.return_value=resp_mock
+    return url
     
 
 def test_get_github_avatar(avatar_url):
